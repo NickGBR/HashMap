3f62f5da42613d113dd9343115d86a06e68f17d7
NickGBR <gubarenko.nikita27@gmail.com>
2020-09-09 21:58:50 +0800

    first "add" version

--- src/HashMap.java
+++ src/HashMap.java
@@ -1,17 +1,48 @@
+import java.lang.invoke.VarHandle;
+
 class HashMap<K,V> {
 
-    private int i = 0;
+    private int capacity = 0;
+    private int counter = 0;
     private Object[] keys;
     private Object[] values;
+    private Object[] previousKeys;
+    private Object[] previousValues;
 
-    public void add(K key, V value) {
-        keys = new Object[3];
-        values = new Object[3];
-        keys[0] = key;
-        values[0] = value;
+    public void put(K key, V value) {
+
+        previousKeys = keys;
+        previousValues = values;
+        capacity++;
+
+        keys = new Object[capacity];
+        values = new Object[capacity];
+
+        if (previousKeys != null && previousValues != null) {
+
+            for (Object a : previousKeys) {
+                keys[counter] = a;
+                counter++;
+            }
+
+
+            counter = 0;
+
+            for (Object a : previousValues) {
+                values[counter] = a;
+                counter++;
+            }
+        }
+
+        keys[capacity - 1] = key;
+        values[capacity - 1] = value;
+
+        counter = 0;
     }
 
     public  void get(){
-        System.out.println(keys[0] + " " + values[0]);
+        for(int i = 0; i<keys.length; i++){
+            System.out.println(keys[i] + " " + values[i]);
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,14 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+        java.util.HashMap<String, Integer> b = new java.util.HashMap<>();
         HashMap<String, Integer> a = new HashMap<>();
-        a.add("lol", 2);
+        a.put("lol", 2);
+        a.put("lol2", 4);
+        a.put("pp", 7);
+        a.put("pp", 7);
         a.get();
+
     }
 }
 
