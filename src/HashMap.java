9f2ee1dfc55b6be8c1af96e7e410b1c91ea1e763
NickGBR <gubarenko.nikita27@gmail.com>
2020-09-09 23:39:37 +0800

    The third verson of  add method

--- src/HashMap.java
+++ src/HashMap.java
@@ -1,6 +1,6 @@
 
 class HashMap<K,V> {
-
+    private boolean isExistinKey;
     private int capacity = 0;
     private int counter = 0;
     private Object[] keys;
@@ -10,39 +10,38 @@ class HashMap<K,V> {
 
     public void put(K key, V value) {
 
-        previousKeys = keys;
-        previousValues = values;
-        capacity++;
+        isExistinKey = isExistingKey(key);
 
-        keys = new Object[capacity];
-        values = new Object[capacity];
+        if(!isExistinKey) {
+            previousKeys = keys;
+            previousValues = values;
 
-        if (previousKeys != null && previousValues != null) {
+            capacity++;
 
-            for (Object a : previousKeys) {
-                keys[counter] = a;
-                counter++;
+            keys = new Object[capacity];
+            values = new Object[capacity];
+
+            if (previousKeys != null && previousValues != null) {
+
+                for (Object a : previousKeys) {
+                    keys[counter] = a;
+                    counter++;
+                }
+
+
+                counter = 0;
+
+                for (Object a : previousValues) {
+                    values[counter] = a;
+                    counter++;
+                }
             }
-
-
-            counter = 0;
-
-            for (Object a : previousValues) {
-                values[counter] = a;
-                counter++;
-            }
-        }
-
-        if (isExistingKey(key)) {
-
-            changeValue(key,value);
-            capacity--;
-
-        } else {
             keys[capacity - 1] = key;
             values[capacity - 1] = value;
         }
-
+        else {
+            changeValue(key, value);
+        }
         counter = 0;
     }
 
@@ -53,12 +52,17 @@ class HashMap<K,V> {
         }
     }
 
-    private void changeValue(Object value) {
-
-    }
+    /**
+     * his method checks the same key existense.
+     * @param key
+     * @return
+     */
 
     private boolean isExistingKey(Object key) {
         int counter = 0;
+        if(keys == null){
+            return false;
+        }
         if (keys.length > 1) {
 
             for (Object a : keys) {
@@ -72,6 +76,12 @@ class HashMap<K,V> {
         }
         return false;
     }
+
+    /**
+     * Method for changing value of key
+     * @param key
+     * @param value
+     */
 
     private void changeValue(Object key, Object value) {
         int counter = 0;
--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,16 @@ public class Main {
         a.put("lol2", 4);
         a.put("pp", 7);
         a.put("pp", 7);
-        a.get();
         a.put("lol", 444);
         a.put("nick", 333);
         a.put("nick", 777);
-        System.out.println("--------");
-        a.get();
+//        System.out.println("--------");
+//        a.get();
         a.put("Green", 222);
-        System.out.println("--------");
-        a.get();
+//        System.out.println("--------");
+//        a.get();
         a.put("Green", 123);
-        System.out.println("--------");
+//        System.out.println("--------");
         a.get();
 
     }
