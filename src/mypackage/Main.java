a571b82c506ccbc8dbc3509ef8dfa1414aff016c
NickGBR <gubarenko.nikita27@gmail.com>
2020-09-16 20:55:12 +0800

    MainCleard
    Deleted some linse in main class

--- src/mypackage/Main.java
+++ src/mypackage/Main.java
@@ -1,77 +1,8 @@
 package mypackage;
-import java.util.Iterator;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
-        java.util.HashMap<String, Integer> b = new java.util.HashMap<String, Integer>();
-        java.util.HashMap<String, Integer> a = new java.util.HashMap<String, Integer>();
-
-        a.put("lol",222);
-        
-        HashMap<String, Integer> map = new HashMap<>();
-        HashMap<String, Integer> map25 = new HashMap<>();
-
-        map25.put("!!!",333);
-        map25.put("!!",2);
-        map25.put("!",3);
-
-        map.put("house",333);
-        map.put("doge",2);
-        map.put("green",3);
-        map.put("eco",5);
-        map.put("sofa",6333333);
-        map.put("length",7);
-        map.put("dad",8);
-        map.put("also",8);
-        map.put("international",8);
-        map.put("blizzard",8);
-        map.put("hello",8);
-        map.put("floor",222);
-        map.put("flap",222);
-        map.put("man",222);
-        map.put("woman",222);
-        map.put("топор",222);
-        map.put("огонь",222);
-        map.put("Как дела",222);
-        map.put("только",222);
-        map.put("Не много",222);
-        map.put("What's uo",222);
-        map.put("a little bit",222);
-
-
-        //map.clear();
-        HashMap<String, Integer> map2 = new HashMap<>();
-
-        map2.put("house",333);
-        map2.put("doge",2);
-        map2.put("green",3);
-        map2.put("eco",5);
-        map2.put("sofa",6333333);
-        map2.put("length",7);
-        map2.put("dad",8);
-        map2.put("also",8);
-        map2.put("international",8);
-        map2.put("blizzard",8);
-        map2.put("hello",8);
-        map2.put("floor",222);
-        map2.put("flap",222);
-        map2.put("man",222);
-        map2.put("woman",222);
-        map2.put("топор",222);
-        map2.put("огонь",222);
-        map2.put("Как дела",222);
-        map2.put("только",222);
-        map2.put("Не много",222);
-        map2.put("What's uo",222);
-        map2.put("a little bit",222);
-
-
-
-        //map.putAll(map25);
-        System.out.println(map);
-
 
     }
 }
